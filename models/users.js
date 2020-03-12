9e3410e7277efc6cbeb6f70d1e92fb086c29572d
Tushar3099 <51849085+Tushar3099@users.noreply.github.com>
2020-03-13 00:46:00 +0530

    Update users.js

--- models/users.js
+++ models/users.js
@@ -3,17 +3,19 @@ const mongoose=require("mongoose");
 const passportLocalMongoose=require("passport-local-mongoose");
 
 var userSchema=new mongoose.Schema({
-    username:String,
-    email:String,
-    password:String,
-    image : { 
-      type : String ,
-      default: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder.jpg"
-    },
-    date: {
-        type: Date,
-        default: Date.now
-      }
+  
+            username:String,
+            email:String,
+            password:String,
+            image : { 
+              type : String ,
+              default: "https://www.bsn.eu/wp-content/uploads/2016/12/user-icon-image-placeholder.jpg"
+            },
+            date: {
+                type: Date,
+                default: Date.now
+              }
+
 })
 
 userSchema.plugin(passportLocalMongoose);
